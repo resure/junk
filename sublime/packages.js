57b716d0235a521e29ba072539a64ef5c691fac9
Gadzhi Gadzhiev <resure@yandex.com>
2016-03-20 01:12:57 +0300

    add sublime packages list

---
+++ sublime/packages.js
@@ -0,0 +1,29 @@
+{
+	"bootstrapped": true,
+	"in_process_packages":
+	[
+	],
+	"installed_packages":
+	[
+		"DocBlockr",
+		"Dotfiles Syntax Highlighting",
+		"EditorConfig",
+		"Evaluate",
+		"FileDiffs",
+		"Git",
+		"GitGutter",
+		"Javascript Beautify",
+		"JavaScriptNext - ES6 Syntax",
+		"Local History",
+		"Markdown Extended",
+		"Missing Palette Commands",
+		"Oceanic Next Color Scheme",
+		"Package Control",
+		"PlainTasks",
+		"SideBarEnhancements",
+		"SublimeLinter",
+		"SublimeLinter-contrib-xo",
+		"Theme - SoDaReloaded",
+		"Theme - Spacegray"
+	]
+}
